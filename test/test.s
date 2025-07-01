af4c4c2ff16d28a0af15e037f621d6b358e9b9e2
Wojciech Polak <wojciech.polak@frondeus.pl>
2025-07-01 20:51:31 +0200

    Use mutually recursive let to define expansion

--- test/test.s
+++ test/test.s
@@ -1,13 +1,19 @@
-(let :fix
-    (fn (:f) (do
-        (let* :result (f result))
-        result
-    ))
-)
 
-(let :result (fix (fn (:self)
-    (obj/plain
+(let :expand (fn (:base :ext) (do
+    (let*
+        :super  (base result)
+        :result (ext result super)
+    )
+    result
+)))
+
+(let :left (fn (:self) (obj/plain
     :x 1
     :y (thunk () (self :x))
-    )
 )))
+
+(let :right (fn (:self :super) (obj/extend super
+    :x 2
+)))
+
+(expand left right)
